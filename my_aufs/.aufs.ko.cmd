9c5bcce9729cc1debebd0a33f7475e1975d0d693
root <root@localhost.(none)>
2016-01-13 17:42:38 -0800

    delete my_aufs middle files

--- my_aufs/.aufs.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/centos-dev/modules/my_aufs/aufs.ko := ld -r -m elf_x86_64 -T /home/centos-dev/kernel/pure_kernel/linux-2.6.36/scripts/module-common.lds --build-id  -o /home/centos-dev/modules/my_aufs/aufs.ko /home/centos-dev/modules/my_aufs/aufs.o /home/centos-dev/modules/my_aufs/aufs.mod.o
--- my_aufs/.tmp_versions/aufs.mod
+++
@@ -1,2 +0,0 @@
-/home/centos-dev/modules/my_aufs/aufs.ko
-/home/centos-dev/modules/my_aufs/fs_test_main.o
